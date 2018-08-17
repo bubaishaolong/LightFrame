99919cd54b1ab12a50bac80fe01adce21c58ed8e
zj4 <zj4@z9168.com>
2018-08-17 17:34:06 +0800

    增加Api的专属文件夹来控制版本

--- application/route.php
+++ application/route.php
@@ -17,5 +17,7 @@ return [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
-
+    // api版本路由
+    'api/:version/:controller'=>'api/:version.:controller/index',// 省略方法名时
+    'api/:version/:controller/:function'=>'api/:version.:controller/:function'// 有方法名时
 ];
